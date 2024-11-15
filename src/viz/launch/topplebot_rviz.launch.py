cefcd414b7cf6393cc450b882ec5ef43ca3e8bee
gabearod2 <gabearod2@gmail.com>
2024-11-14 21:35:15 -0500

    Rviz visualization edits

--- src/viz/launch/topplebot_rviz.launch.py
+++ src/viz/launch/topplebot_rviz.launch.py
@@ -1,18 +1,28 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    # Get the path to the URDF file
+    package_dir = get_package_share_directory('viz')
+    urdf_file = os.path.join(package_dir, 'urdf', 'topplebot.urdf')
+
+    # Read the URDF file
+    with open(urdf_file, 'r') as file:
+        robot_description = file.read()
+
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': open('src/viz/topplebot.urdf').read()}]
+            parameters=[{'robot_description': robot_description}]
         ),
         Node(
             package='viz',
-            executable='imu_to_tf_broadcaster',
+            executable='tb_tf_broadcaster',
             name='imu_to_tf_broadcaster',
             output='screen'
         ),
@@ -21,6 +31,6 @@ def generate_launch_description():
             executable='rviz2',
             name='rviz2',
             output='screen',
-            arguments=['-d', 'src/viz/config/topplebot.rviz']
+            arguments=['-d', os.path.join(package_dir, 'config', 'topplebot.rviz')]
         )
     ])
--- src/viz/setup.py
+++ src/viz/setup.py
@@ -1,27 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+from glob import glob
 
 package_name = 'viz'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*.rviz')),
+        ('share/' + package_name + '/urdf', glob('urdf/*.urdf')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Gabriel Rodriguez',
-    maintainer_email='gabearod2@gmail.com',
-    description='ToppleBot RViz visualization support',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer_email='your.email@example.com',
+    description='Visualization package for ToppleBot',
+    license='Apache License 2.0',
     entry_points={
-    'console_scripts': [
-        'imu_to_tf_broadcaster = viz.topplebot_tf_broadcaster:main',
-    ],
-}
-,
+        'console_scripts': [
+            'tb_tf_broadcaster = viz.tb_tf_broadcaster:main',
+        ],
+    },
 )

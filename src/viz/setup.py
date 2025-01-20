389d7d95e8dcb6611ba83045cef47ed84abd2924
gabearod2 <gabearod2@gmail.com>
2025-01-20 13:18:18 -0500

    Added odometry capabilites, NOT FUNCTIONAL, but is RUNNNING

--- src/viz/setup.py
+++ src/viz/setup.py
@@ -8,6 +8,7 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/viz']),  # Corrected path
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
         ('share/' + package_name + '/config', glob('config/*.rviz')),
--- src/viz/viz/tb_tf_broadcaster.py
+++ src/viz/viz/tb_tf_broadcaster.py
@@ -6,10 +6,21 @@ from sensor_msgs.msg import Imu
 from geometry_msgs.msg import TransformStamped
 import tf2_ros
 
-class IMUToTFNode(Node):
+'''
+This node publishes the Odometry for the robot. 
+
+Needs the following debugging:
+TODO: Ensure the quaternion is handled correctly
+TODO: Add logging at each step
+TODO: Edit urdf so that all frames are visualized
+TODO: Ensure the calibration axes work correctly
+'''
+
+
+class OdometryNode(Node):
     def __init__(self):
-        super().__init__('imu_to_tf_broadcaster')
-        self.first_quaternion_logged = False
+        super().__init__('odometry_broadcaster')
+        self.first_run = True
         
         # Subscribe to the imu_data topic
         self.subscription = self.create_subscription(
@@ -20,63 +31,168 @@ class IMUToTFNode(Node):
         
         # TF broadcaster
         self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
-        self.get_logger().info("IMU to TF broadcaster started")
+        self.get_logger().info("Odometry broadcaster started")
+
+        # Initial node positions in the world frame
+        self.length = 0.05 #[m]
+        self.initial_node_positions = np.array([ 
+            [0,             0,             0            ], # node 0
+            [0,             self.length,   0            ], # node 1
+            [0,             self.length,   self.length  ], # node 2
+            [0,             0,             self.length  ], # node 3
+            [self.length,   0,             0            ], # node 4
+            [self.length,   self.length,   0            ], # node 5
+            [self.length,   self.length,   self.length  ], # node 6
+            [self.length,   0,             self.length  ], # node 7
+            [self.length/2, self.length/2, self.length/2]  # base_link node 
+        ]) # 9 Rows, 3 Columms
+
+        # Initializing variables
+        self.bl_g = np.zeros((1,3))
+        self.q = np.zeros((1,4))
+        self.bl_node_vectors = np.zeros((8, 3))# in the base_link frame
+        self.bl_node_vectors_diffs = np.zeros((9, 3))
+        self.bn_node_positions = np.zeros((8,3))
+        self.n0_node_positions = np.zeros((1,3))
+        self.topple_translation = np.zeros((1,3)) # world frame
+        self.current_topple_translation = np.zeros((1,3))
+        self.last_bn_index = 0 # initial balancing node index
 
     def imu_callback(self, msg):
-        # Create a TransformStamped message
+
+        ### Getting the current quaternion ###
+        self.q = np.array([
+            msg.orientation.x,
+            msg.orientation.y,
+            msg.orientation.z,
+            msg.orientation.w
+        ])
+
+        ### Handling the Initial Position ###
+
+        # Initializing with node 0 as the balancing node, world and bn frames are aligned.
+        if self.first_run: 
+            self.bn_node_positions = self.initial_node_positions
+            self.first_run = False
+
+        ### Determining the Current Balancing Node ###
+        
+        # First getting the gravity vector in the base_link frame
+        # Note: We are assuming it initializes w/ a q = [0,0,0,1], if not make it so through better calibration
+        # TODO: VERIFY THIS IS THE CASE. 
+        self.bl_g = np.array([
+            msg.linear_acceleration.x,
+            msg.linear_acceleration.y,
+            msg.linear_acceleration.z])
+        self.get_logger().info(f'Updated Linear Acceleration Vector: {self.bl_g}') 
+        
+        #-np.linalg.norm([self.length/2, self.length/2])
+
+        # Find the vectors from base_link node to each node of the cube in the base_link frame
+        for i in range(8):
+            self.bl_node_vectors[i] = self.bn_node_positions[i,:] - self.bn_node_positions[8,:]
+            self.get_logger().info(f'Node Vectors: {self.bl_node_vectors}') 
+            self.bl_node_vectors_diffs = self.bl_node_vectors[i] - self.bl_g[0]
+
+        
+        # Finding the balancing node
+        self.bn_index = np.argmin(self.bl_node_vectors_diffs)
+
+        ### Determining node positions in the world frame from the Balancing Node ###
+
+        # Determine if there is a translation and keep adding them up.
+        if self.bn_index != self.last_bn_index:
+            self.current_topple_translation = self.bn_node_positions[self.bn_index] - self.bn_node_positions[self.last_bn_index]
+            print(self.current_topple_translation)
+            self.get_logger().info(f'Current Topple Translation: {self.current_topple_translation}')
+            self.bn_node_positions = self.bn_node_positions - self.current_topple_translation
+            self.get_logger().info(f'Updated BN Node Positions: {self.bn_node_positions}') 
+            self.topple_translation += self.current_topple_translation
+
+        ### Update Node Positions based on Current Orientation ###
+
+        for i in range(9):
+            self.bn_node_positions[i] = self.rotate_vec(self.q, self.bn_node_positions[i])
+
+        # Broadcasting the transforms
+        self.translation = self.topple_translation.flatten().tolist()
+
+        self.broadcast_transform('node_0', 'base_link',self.initial_node_positions[8,:],  self.q)
+        self.broadcast_transform('balancing_node', 'node_0',self.bn_node_positions[0,:])
+        self.broadcast_transform('world', 'balancing_node',self.translation)
+
+    def broadcast_transform(self, parent_frame, child_frame, translation, rotation=None):
+        """Broadcast a transform between two frames."""
         transform = TransformStamped()
         transform.header.stamp = self.get_clock().now().to_msg()
-        transform.header.frame_id = 'world'  # Updated parent frame
-        transform.child_frame_id = 'base_link'   # Updated child frame
+        transform.header.frame_id = parent_frame
+        transform.child_frame_id = child_frame
 
-        # x y z
-        self.initial_translation_vector = np.array([0.0635, 0.0635, 0.0635])
-
-        # Getting the current quaternion
-        self.current_quat = np.array([
-            msg.orientation.y,
-            msg.orientation.x,
-            -msg.orientation.z,
-            msg.orientation.w])
-
-        # if the first loop, log the current orientation as the first 
-        if not self.first_quaternion_logged:
-            self.first_quaternion = self.current_quat
-            self.first_quaternion_logged = True
+        # Setting the translation vector
+        transform.transform.translation.x = translation[0]
+        transform.transform.translation.y = translation[1]
+        transform.transform.translation.z = translation[2]
         
-        # Get the initial rotation
-        self.initial_r = R.from_quat(self.first_quaternion)
+        # Use identity rotation if not specified
+        if rotation is None:
+            rotation = np.array([0.0, 0.0, 0.0, 1.0])
 
-        # Get the inverse of that initial rotation
-        self.initial_inv_r = self.initial_r.inv()
+        rotation = [float(r) for r in rotation]
+        
+        # Setting the rotation (quaternion)
+        transform.transform.rotation.x = rotation[0]
+        transform.transform.rotation.y = rotation[1]
+        transform.transform.rotation.z = rotation[2]
+        transform.transform.rotation.w = rotation[3]
 
-        # Get rotation from euler of 90 deg
-
-        # Getting the current rotation, neglecting the inital IMU orientation
-        self.current_r = R.from_quat(self.current_quat)*self.initial_inv_r
-
-        # Applying the rotation to the intial translation vector
-        self.current_transform = self.current_r.apply(self.initial_translation_vector)
-
-        # Getting the quaternion from the current orientation - scalar last as default
-        self.current_orientation = R.as_quat(self.current_r)
-
-        # Setting the actual transformation now
-        transform.transform.rotation.x = self.current_orientation[0]
-        transform.transform.rotation.y = self.current_orientation[1]
-        transform.transform.rotation.z = self.current_orientation[2]
-        transform.transform.rotation.w = self.current_orientation[3]
-
-        transform.transform.translation.x = self.current_transform[0]
-        transform.transform.translation.y = self.current_transform[1]
-        transform.transform.translation.z = self.current_transform[2]
-
-        # Broadcast the transform
         self.tf_broadcaster.sendTransform(transform)
+    
+    def rotate_vec(self, q, v):
+        """
+        Rotate a vector, v, using a unit quaternion q.
+        
+        Params:
+        - q: numpy array of shape (4,), the unit quaternion (x, y, z, w)
+        - v: numpy array of shape (3,), the 3D vector to rotate
+        
+        Returns:
+        - v_rot: numpy array of shape (3,), the 3D rotated vector
+        """
+        # Normalize the quaternion to ensure it is a unit quaternion
+        q = q / np.linalg.norm(q)
+
+        # Extract components of the quaternion
+        x, y, z, w = q
+
+        # Compute intermediate values
+        # This avoids creating additional temporary quaternions
+        ww = w * w
+        xx = x * x
+        yy = y * y
+        zz = z * z
+        wx = w * x
+        wy = w * y
+        wz = w * z
+        xy = x * y
+        xz = x * z
+        yz = y * z
+
+        # Construct the rotation matrix derived from the quaternion
+        rot_matrix = np.array([
+            [ww + xx - yy - zz, 2 * (xy - wz), 2 * (xz + wy)],
+            [2 * (xy + wz), ww - xx + yy - zz, 2 * (yz - wx)],
+            [2 * (xz - wy), 2 * (yz + wx), ww - xx - yy + zz]
+        ])
+
+        # Rotate the vector
+        v_rot = np.dot(rot_matrix, v)
+
+        return v_rot
+
 
 def main(args=None):
     rclpy.init(args=args)
-    node = IMUToTFNode()
+    node = OdometryNode()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
